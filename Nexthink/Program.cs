2a023723ae5a1c3a59f75508b0f776b39a6026da
nourdandan <nour.dandan@gmail.com>
2020-06-25 11:03:32 +0200

    presentation layer on console

--- Nexthink/Program.cs
+++ Nexthink/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Nexthink.BusinessLayer;
+using Nexthink.Parsers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,34 @@ namespace Nexthink
     {
         static void Main(string[] args)
         {
-            var folderPath = @"..\Data.csv";
+            var folderPath = @"C:\Users\n.dandan\Documents\PowershellSoftwareDeveloper";
+            var files = Directory.GetFiles(folderPath);
+           
+            foreach (var fileName in files)
+            {
+                DataFilter filter;
+                switch (Path.GetExtension(fileName).ToString())
+                {
+                    case ".xml":
+                         filter = new DataFilter(new XmlParser(fileName, true));
+                        PrintValues(filter.GetWithNumberOfMonitoLarger(1),"xml");
+                        break;
+                    case ".csv":
+                        filter = new DataFilter(new CsvParser(fileName, true));
+                        PrintValues(filter.GetWithNumberOfMonitoLarger(1), "csv");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void PrintValues(List<Tuple<string,string>> devices,string fileType)
+        {
+            foreach (var tuple in devices)
+            {
+                Console.WriteLine($"FileType : {fileType}, Device name : {tuple.Item1} , Last IP : {tuple.Item2}");
+            }
         }
     }
 }
